e62a8ad11b7f900d1b446f04c50e053a732152ae
haiderhadi055 <haiderhadi055@gmail.com>
2023-11-04 07:01:15 +0500

    feat: minimal rust kernel

---
+++ readme.md
@@ -0,0 +1,11 @@
+# Run Cargo Build with the command
+
+`cargo build`
+
+# To create the bootable disk image
+
+`cargo bootimage`
+
+# To run the OS
+
+`qemu-system-x86_64 -drive format=raw,file=C:\Users\haide\Documents\Development\Rust_OS\target\x86_64-blog_os\debug\bootimage-Rust_OS.bin`
--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,21 @@
 
 use core::panic::PanicInfo;
 
+static HELLO: &[u8] = b"Hello World!";
+
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
     // this function is the entry point, since the linker looks for a function
     // named `_start` by default
+    let vga_buffer = 0xb8000 as *mut u8; // mutable raw pointer to a single byte
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte; // write the byte to the correct location
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb; // set the color byte
+        }
+    }
+
     loop {}
 }
 
