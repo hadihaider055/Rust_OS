8e8d99a65d1dcc3e828d7de9c2412533b93a01f0
haiderhadi055 <haiderhadi055@gmail.com>
2023-11-05 22:54:22 +0500

    feat: VGA Text Mode

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
 
+mod vga_buffer;
+
 use core::panic::PanicInfo;
 
 static HELLO: &[u8] = b"Hello World!";
@@ -9,20 +11,23 @@ static HELLO: &[u8] = b"Hello World!";
 pub extern "C" fn _start() -> ! {
     // this function is the entry point, since the linker looks for a function
     // named `_start` by default
-    let vga_buffer = 0xb8000 as *mut u8; // mutable raw pointer to a single byte
+    // let vga_buffer = 0xb8000 as *mut u8; // mutable raw pointer to a single byte
+    println!("Hello World{}", "!");
+    panic!("I am panicking huhuhuhu!!!");
 
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte; // write the byte to the correct location
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb; // set the color byte
-        }
-    }
+    // for (i, &byte) in HELLO.iter().enumerate() {
+    //     unsafe {
+    //         *vga_buffer.offset(i as isize * 2) = byte; // write the byte to the correct location
+    //         *vga_buffer.offset(i as isize * 2 + 1) = 0xb; // set the color byte
+    //     }
+    // }
 
     loop {}
 }
 
 /// This function is called on panic.
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
